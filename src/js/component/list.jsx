59a96d05a0b2097d4e6802a8b034f147d756e3b9
Vasily-drov <vasily.drov@gmail.com>
2022-10-30 14:27:33 +0000

    one comp

--- src/js/component/list.jsx
+++ src/js/component/list.jsx
@@ -1,44 +1,43 @@
 import React, {useState} from "react";
-import Task from "./task.jsx";
 
 //include images into your bundle
 
 //create your first component
 const List = () => {
-	let [inputValue, setInputValue] = useState([])
+
+	let [tasks, setTasks] = useState([])
 
 	let pushValue = (e) => {
 		e.preventDefault();
 		let clearInput = e.target[0].value;
-		console.log(e.target[0].value);
-		setInputValue((current) => {
+		setTasks((current) => {
 		  return [...current, clearInput];
 		});
 		e.target[0].value = "";
 	}
 
     function deleteTask(id) {
-        console.log('clicked')
-        let a = 'sdfsf'
-        setInputValue((current)=>{
+        setTasks((current)=>{
             console.log(current)
             return current.filter((item, index)=>{
               return index != id
             })
           })
       }
-	
-	
 
 	return (
 		<div>
-		<form onSubmit={pushValue}>
-		  <input />
-		  <button >+</button>
+			<form onSubmit={pushValue}>
+		  	<input />
+		  	<button >+</button>
 		</form>
 		<ul>
-		  {inputValue.map((task, index) => {
-			return <Task id = {index} key={index} task={task} onDelete={deleteTask}/>;
+		  {tasks.map((task, index) => {
+			return <div >
+					<li id = {index} key={index} >{task} 
+						<button onClick={()=>deleteTask(index)}>X</button>
+					 </li>
+				</div>;
 		  })}
 		</ul>
 	  </div>
